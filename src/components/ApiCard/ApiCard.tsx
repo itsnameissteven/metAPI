dee03a353d3f38ced5ed421bd8134fa8b38a977d
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-12 15:47:35 -0400

    Create ApiCard, acting as link

--- src/components/ApiCard/ApiCard.tsx
+++ src/components/ApiCard/ApiCard.tsx
@@ -1,15 +1,18 @@
-import {Api} from '../../apiCalls'
-
+import {Api} from '../../apiCalls';
+import {Link} from 'react-router-dom'
+const CardLink = Link
 export const ApiCard = ({API, Auth, Cors, HTTPS, Category, Description, Link}: Api) => {
   return (
-    <article>
-      <h2>{API}</h2>
-      <p>{Auth}</p>
-      <p>{Cors}</p>
-      <p>{HTTPS}</p>
-      <p>{Category}</p>
-      <p>{Description}</p>
-      <p>{Link}</p>
-    </article>
+    <CardLink to={'/' + API}>
+      <article>
+        <h2>{API}</h2>
+        <p>{Auth}</p>
+        <p>{Cors}</p>
+        <p>{HTTPS}</p>
+        <p>{Category}</p>
+        <p>{Description}</p>
+        <p>{Link}</p>
+      </article>
+    </CardLink>
     )
 }
