4a9d6372824148cc40d90c569cc0fd2ab5ce4df1
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-12 19:07:31 -0400

    Add a cardlink comment explanation

--- src/components/ApiCard/ApiCard.tsx
+++ src/components/ApiCard/ApiCard.tsx
@@ -1,5 +1,8 @@
 import {Api} from '../../apiCalls';
 import {Link} from 'react-router-dom'
+/**
+ * Representing a router link because the destructured Link prop was reassigning the value of Link
+ */
 const CardLink = Link
 export const ApiCard = ({API, Auth, Cors, HTTPS, Category, Description, Link}: Api) => {
   return (
