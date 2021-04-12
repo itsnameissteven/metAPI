aefc2f2788351557790f34c73c62def52c0f4524
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-12 12:59:40 -0400

    Create initial App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getApis, Api } from './apiCalls'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+type Props = {}
+
+class App extends React.Component<Props> {
+  state : {
+    apiList: Api[]
+  };
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      apiList: []
+    }
+  }
+
+  componentDidMount() {
+    getApis()
+    .then(data => this.setState({apiList: data.entries})) 
+  }
+
+  render() {
+     console.log(this.state)
+    return (
+      <div className="App">
     </div>
   );
+}
 }
 
 export default App;
