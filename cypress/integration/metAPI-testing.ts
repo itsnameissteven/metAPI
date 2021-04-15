24302f2be2ba1bd85fcc396ab4222e46a1838a29
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-15 19:33:10 -0400

    Add Initial sidebar and form tests

--- cypress/integration/metAPI-testing.ts
+++ cypress/integration/metAPI-testing.ts
@@ -35,4 +35,47 @@ describe('metAPI', () => {
 
   })
 
+  describe('Sidebar', () => {
+    
+    beforeEach(() => {
+      cy.visit('http://localhost:3000');
+      cy.get('.hamburger').click()
+    })
+
+    it('Should allow a user to view their saved cards in the sidebar', () => {
+      cy.get('main').find('.favorite-btn__heart').first().click()
+      cy.get('.side-bar').find('.api-card')
+    })
+
+    it('Should allow a user to click on a saved card to view the details page', () => {
+      cy.get('main').find('.favorite-btn__heart').first().click()
+      cy.get('.side-bar').find('.api-card').click()
+      cy.get('.featured-card').contains('Cat Facts')
+    })
+
+    it('Should start out with no saved cards', () => {
+      cy.get('.side-bar').find('.api-card').should('not.exist')
+    })
+  })
+
+  describe.only('Filter form', () => {
+
+    beforeEach(() => {
+      cy.visit('http://localhost:3000')
+    })
+
+    it('Should show a user how many options are available for each category', () => {
+      cy.get('form').find('.category').first().contains('Animals(13)')
+    })
+
+    it('Should allow a user to filter by category', () => {
+      cy.get('form').get('.category').first().find('label').click()
+      cy.get('.api-card').should('have.length', 13)
+    })
+
+    it('Should allow a user to filter by search', () => {
+      cy.get('form').find('.search-bar').type('cat').should('have.value', 'cat').wait(1000)
+    })
+  })
+
 })
