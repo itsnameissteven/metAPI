0b849c95e6e7b5cdd6bff590cf974393e2b47c27
Steven Mancine <stevenrmancine@gmail.com>
2021-04-15 17:21:15 -0600

    Add test to write and delete test

--- cypress/integration/metAPI-testing.ts
+++ cypress/integration/metAPI-testing.ts
@@ -49,7 +49,7 @@ describe('Selected API page', () => {
   })
 
   it('Should be able to click on an api to see more information', () => {
-    cy.get(".api-card-link[href='/api/What Anime']").click()
+    cy.get('.api-card-link[href="/api/What Anime"]').click()
       .get('.featured-card').get('.home-link').contains('View all apis')
       .get('.featured-card').contains('What Anime')
       .get('.featured-card').contains('Scan anime')
@@ -58,7 +58,28 @@ describe('Selected API page', () => {
       .get('.featured-card').contains('Https: yes')
       .get('.featured-card').contains('Category: Anime')
       .get('.featured-card').get('.featured-card__btn')
-        .get('.external-link').should('have.attr', 'href', "https://soruly.github.io/trace.moe/")
+        .get('.external-link').should('have.attr', 'href', 'https://soruly.github.io/trace.moe/')
       .get('.featured-card').get('.save-note__btn').contains('Save Note')
+  })
+
+  it('Should be able to save a note', () => {
+    cy.visit('http://localhost:3000/api/What%20Anime')
+      .get('.featured-card__notes').type('Create an app where Anyone can scan in an anime character')
+      .get('.save-note__btn').click()
+      .get('.note').contains('Create an app where Anyone can scan in an anime character')
+  })
+
+  it('Should not be able to save a note if the note is empty', () => {
+    cy.visit('http://localhost:3000/api/What%20Anime')
+      .get('.save-note__btn').click()
+      .get('.note').should('not.exist')
+  })
+
+  it('Should be able to delete a note', () => {
+    cy.visit('http://localhost:3000/api/What%20Anime')
+      .get('.featured-card__notes').type('ops ths note ahs to may typos')
+      .get('.save-note__btn').click()
+      .get('.note').get('.delete-btn').click()
+      .get('.note').should('not.exist')
   })
 })
