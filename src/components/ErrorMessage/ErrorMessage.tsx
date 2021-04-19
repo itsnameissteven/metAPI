0785f496eaca619c721d263bc94bbb54b2da729e
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-04-19 10:03:27 -0600

    Update error message component

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineExclamationCircle } from "react-icons/ai"
 
 export const ErrorMessage = (statusCode: any) => {
   if (parseInt(statusCode.statusCode) < 500) {
     return (
-      <div>
-        <h1>This is a 400 error</h1>
+      <div className="error-message-container">
+        <div className="error-message">
+          <AiOutlineExclamationCircle className="warning-icon"/>
+          <h2>Sorry, we couldn't find the page you're looking for.</h2>
+          <button className="error-button" onClick={() => window.location.href="/"}>Go back</button>
+        </div>
       </div>
     ) 
   } else {
     return (
-      <div>
-        <h1>This is a 500 error</h1> 
+      <div className="error-message-container">
+        <div className="error-message">
+          <AiOutlineExclamationCircle className="warning-icon" />
+          <h2>Sorry, there was a problem. Please try again later</h2>
+          <button className="error-button" onClick={() => window.location.href="/"}>Try again</button>
+        </div>    
       </div>
     ) 
   }
